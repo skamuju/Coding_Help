40df7cbc8fda1870cb13b5b1073a14b71d8df4f5
skamuju <63597158+skamuju@users.noreply.github.com>
2020-05-20 15:22:22 -0400

    n_max()

--- sort.py
+++ sort.py
@@ -1,26 +1,39 @@
 import random
-
 num_list = []
-
 num_gen = 1
-
 for x in range(1000):
     x += 1
     num_list.append(x)
-
 rand_set = random.sample(num_list, 4)
- 
-print(rand_set)
+
 
 def maximum():
-    return max(rand_set)
+    return max(random.sample(num_list, 4))
 
 def max_order():
     order_l = []
+    copy_rand = random.sample(num_list, 4)
     for x in range(4):
-        order_l.append(max(rand_set))
-        rand_set.remove(max(rand_set))
+        order_l.append(max(copy_rand))
+        copy_rand.remove(max(copy_rand))
     return order_l
 
+def min_order():
+    order_l2 = []
+    copy_rand2 = random.sample(num_list, 4)
+    for x in range(4):
+        order_l2.append(min(copy_rand2))
+        copy_rand2.remove(min(copy_rand2))
+    return order_l2
+def n_max():
+    order_l3 = []
+    copy_rand3 = random.sample(num_list, 4)
+    for x in range(4):
+        order_l3.append(max(copy_rand3))
+        copy_rand3.remove(max(copy_rand3))
+    n = int(input())-1
+    return order_l3,order_l3[n]
+
 print(max_order())
-#print(maximum())
+print(min_order())
+print(n_max())
