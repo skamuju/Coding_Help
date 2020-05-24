fbc212fd1a31843bbb6855fd87d5e7091d8cba53
skamuju <63597158+skamuju@users.noreply.github.com>
2020-05-24 11:46:11 -0400

    Summary (required)

--- sort.py
+++ sort.py
@@ -10,11 +10,13 @@ def maximum():
 
 def max_order():
     order_l = []
+    og_sample = []
     copy_rand = random.sample(num_list, 4)
+    og_sample.append(copy_rand)
     for x in range(4):
         order_l.append(max(copy_rand))
         copy_rand.remove(max(copy_rand))
-    return order_l
+    return og_sample, order_l
 
 def min_order():
     order_l2 = []
@@ -31,8 +33,7 @@ def n_max():
         order_l3.append(max(copy_rand3))
         copy_rand3.remove(max(copy_rand3))
     n = int(input("n = "))-1
-    return order_l3[n]
+    return order_l3, order_l3[n]
 
 print(max_order())
-print(min_order())
-print(n_max())
+
